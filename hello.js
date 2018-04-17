c5876fcc046d0bc89f64dd96ec95f73515de4928
Wael <waelhom2@gmail.com>
2018-04-18 00:08:05 +0200

    initial commit

---
+++ hello.js
@@ -0,0 +1,6 @@
+
+const hello = 'hello world!';
+const part = hello.slice(0, 5);
+const upper = hello.toUpperCase();
+const startWithHello = hello.startsWith('hello');
+console.log(part[0], upper, startWithHello);
