2f7f752021e8cdf6689499801f9af2e5b0e4bbe4
Wael-Alhomsi <waelhom2@gmail.com>
2018-04-21 22:47:34 +0200

    Updating the file

--- hello.js
+++ hello.js
@@ -1,9 +1,3 @@
-
-const hello = 'hello world!';
-const part = hello.slice(0, 5);
-const upper = hello.toUpperCase();
-const startWithHello = hello.startsWith('hello');
-console.log(part[0], upper, startWithHello);
-const age = 3;
-const months = ['January' , 'February', 'March'];
-console.log(months[2], age);
+const hel = 'hell';
+console.log(hel);
+console.log(hel.startsWith('h'));
