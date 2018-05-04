c2385daa655eb56d2df8f655329e5e36e2cd88c9
Wael-Alhomsi <waelhom2@gmail.com>
2018-05-04 23:44:06 +0200

    made some corrections

--- week3/11-more.js
+++ week3/11-more.js
@@ -2,7 +2,6 @@
 const x = [1, 2, 3];
 const y = [1, 2, 3];
 const z = y;
-
 console.log(x == y ? 'equal' : 'not equal');
 console.log(x === y ? 'equal' : 'not equal');
 console.log(z === y ? 'equal' : 'not equal');
